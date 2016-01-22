200aa598c60a76f97a0feb069118e64f60d32433
Sam Fok <sambfok@gmail.com>
2016-01-22 10:48:43 -0800

    move setup bash script

--- act/setup.bash
+++ act/setup.bash
@@ -3,6 +3,6 @@ SCRIPT_PATH="${BASH_SOURCE[0]}";
 pushd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd -P`;
 popd  > /dev/null
-	
-export ACT_PATH="$SCRIPT_PATH"
-export ACT_HOME="$SCRIPT_PATH"
+
+export ACT_PATH="$SCRIPT_PATH:$SCRIPT_PATH/lib"
+export ACT_HOME="$SCRIPT_PATH:$SCRIPT_PATH/lib"
