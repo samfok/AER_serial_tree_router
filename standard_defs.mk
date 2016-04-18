cd57e1eb1a2c3f6520e94738c4ef8dbf7f928ee8
Sam Fok <sambfok@gmail.com>
2016-04-17 17:02:28 -0700

    automate testing lib

---
+++ standard_defs.mk
@@ -0,0 +1,7 @@
+# This file contains variables common to all Makefiles in the subdirectories.
+
+# Standard changes to fonts used by the Makefiles in the subdirectories
+
+redfont=`tput setaf 1`
+greenfont=`tput setaf 2`
+resetterm=`tput sgr0`
