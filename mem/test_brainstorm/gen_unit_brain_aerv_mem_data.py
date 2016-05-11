81b6752eb5f9c893e4ee2c9d4626628d59f6866e
Sam Fok <sambfok@gmail.com>
2016-05-11 12:12:49 -0700

    config mem 8->16 col, pass deadlock tests
    still need work to pass unit tests

--- mem/test_brainstorm/gen_unit_brain_aerv_mem_data.py
+++ mem/test_brainstorm/gen_unit_brain_aerv_mem_data.py
@@ -11,8 +11,8 @@ mem_write_out = []
 for i in xrange(N):
   mem_in += [i]
   mem_row_select_out += [i % 8]
-  mem_col_select_out += [(i/8) % 8]
-  mem_write_out += [i / 64]
+  mem_col_select_out += [(i/8) % 16]
+  mem_write_out += [i / 128]
 
 mem_in = np.array(mem_in) + OFFSET
 mem_row_select_out = np.array(mem_row_select_out)
@@ -23,3 +23,8 @@ np.savetxt('unit_brain_aerv_mem.in', mem_in, '%d')
 np.savetxt('unit_brain_aerv_mem_row_select.out', mem_row_select_out, '%d')
 np.savetxt('unit_brain_aerv_mem_col_select.out', mem_col_select_out, '%d')
 np.savetxt('unit_brain_aerv_mem_write.out', mem_write_out, '%d')
+
+np.savetxt('unit_bs_aerv_mem.in', mem_in, '%d')
+np.savetxt('unit_bs_aerv_mem_row_select.out', mem_row_select_out, '%d')
+np.savetxt('unit_bs_aerv_mem_col_select.out', mem_col_select_out, '%d')
+np.savetxt('unit_bs_aerv_mem_write.out', mem_write_out, '%d')
