5f86d014a9a0ce451374c23c049f45b433fd805a
Sam Fok <sambfok@gmail.com>
2016-06-26 20:26:09 -0700

    align config memory with existing schematic. revise Makefiles
    Makefiles now check for error during run of prsim not just after

--- mem/test_brainstorm/gen_unit_brain_aerv_mem_data.py
+++ mem/test_brainstorm/gen_unit_brain_aerv_mem_data.py
@@ -2,24 +2,24 @@
 import numpy as np
 
 OFFSET = 512
-N = 256
+N = 256 
 
 mem_in = []
 mem_row_select_out = []
 mem_col_select_out = []
-mem_write_out = []
+mem_bl_out = []
 for i in xrange(N):
   mem_in += [i]
   mem_row_select_out += [i % 8]
   mem_col_select_out += [(i/8) % 8]
-  mem_write_out += [i / 64]
+  mem_bl_out += [i/8*2%16 +(i%128)/64]
 
 mem_in = np.array(mem_in) + OFFSET
 mem_row_select_out = np.array(mem_row_select_out)
 mem_col_select_out = np.array(mem_col_select_out)
-mem_write_out = np.array(mem_write_out)
+mem_bl_out = np.array(mem_bl_out)
 
 np.savetxt('unit_brain_aerv_mem.in', mem_in, '%d')
 np.savetxt('unit_brain_aerv_mem_row_select.out', mem_row_select_out, '%d')
 np.savetxt('unit_brain_aerv_mem_col_select.out', mem_col_select_out, '%d')
-np.savetxt('unit_brain_aerv_mem_write.out', mem_write_out, '%d')
+np.savetxt('unit_brain_aerv_mem_bl.out', mem_bl_out, '%d')
